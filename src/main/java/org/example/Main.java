b83fd259264b1e501036a7d3b06fcb631e4cf570
prince raj <prince.raj@salescode.ai>
2024-04-29 11:40:42 +0530

    test input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         // Load properties file
         Properties properties = new Properties();
-        try (InputStream inputStream = Main.class.getResourceAsStream("/config.properties")) {
+        try (InputStream inputStream = Main.class.getResourceAsStream("/testJens.properties")) {
             properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
@@ -17,6 +17,8 @@ public class Main {
         // Access and print the value of the s3key property
         String s3key = properties.getProperty("s3key");
         System.out.println("s3key value: " + s3key);
+
+//        System.out.println("Loading properties from: " + Main.class.getResource("/testJens.properties"));
 
         // Your existing code
         System.out.println("Hello world!");
