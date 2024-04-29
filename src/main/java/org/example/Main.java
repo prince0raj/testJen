60c7f074b7219c414a9e779e57bfed31a4a339fa
prince raj <prince.raj@salescode.ai>
2024-04-29 12:16:06 +0530

    geting back innit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,15 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
         // Your existing code
-//        if (args.length > 0) {
-//            // Print all command-line arguments
-//            System.out.println("Command-line arguments:");
-//            for (String arg : args) {
-//                System.out.println(arg);
-//            }
-//        } else {
-//            System.out.println("No command-line arguments provided.");
-//        }
+        if (args.length > 0) {
+            // Print all command-line arguments
+            System.out.println("Command-line arguments:");
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+        } else {
+            System.out.println("No command-line arguments provided.");
+        }
         System.out.println("Hello world!");
     }
 }
